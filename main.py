f55887b80477acda5d24fd2af13ac659f4013048
Alessandro Queiroz <alessandro.z.queiroz@gmail.com>
2023-02-11 10:15:14 -0300

    v3.1

--- main.py
+++ main.py
@@ -42,14 +42,17 @@ def main():
     # create instance of BinanceWebSocketApiManager and provide the function for stream processing
     binance_websocket_api_manager = BinanceWebSocketApiManager(BinanceWebSocketApiProcessStreams.process_stream_data, exchange="binance.com-futures") #output_default: set to "dict" to convert the received raw data to a python dict, set to "UnicornFy"
     #{'kline_1m', 'kline_5m', 'kline_15m', 'kline_30m', 'kline_1h', 'kline_12h', 'aggTrade', 'depth@100ms'}
-    binance_websocket_api_manager.create_stream('kline_1m', banco.markets, stream_label='kline_1m')#{'bnbusdt','wavesusdt', 'btcusdt'}
-    binance_websocket_api_manager.create_stream('kline_5m', banco.markets, stream_label='kline_5m')
-    binance_websocket_api_manager.create_stream('kline_15m', banco.markets, stream_label='kline_15m')
-    binance_websocket_api_manager.create_stream('kline_30m', banco.markets, stream_label='kline_30m')
-    binance_websocket_api_manager.create_stream('kline_1h', banco.markets, stream_label='kline_1h')
-    binance_websocket_api_manager.create_stream('kline_12h', banco.markets, stream_label='kline_12h')
-    binance_websocket_api_manager.create_stream('aggTrade', banco.markets, stream_label='aggTrade')
-    binance_websocket_api_manager.create_stream('depth@100ms', banco.markets, stream_label='depth@100ms')
+    # binance_websocket_api_manager.create_stream('kline_1m', banco.markets, stream_label='kline_1m')#{'bnbusdt','wavesusdt', 'btcusdt'}
+    # binance_websocket_api_manager.create_stream('kline_5m', banco.markets, stream_label='kline_5m')
+    # binance_websocket_api_manager.create_stream('kline_15m', banco.markets, stream_label='kline_15m')
+    # binance_websocket_api_manager.create_stream('kline_30m', banco.markets, stream_label='kline_30m')
+    # binance_websocket_api_manager.create_stream('kline_1h', banco.markets, stream_label='kline_1h')
+    # binance_websocket_api_manager.create_stream('kline_12h', banco.markets, stream_label='kline_12h')
+    # binance_websocket_api_manager.create_stream('aggTrade', banco.markets, stream_label='aggTrade')
+    # binance_websocket_api_manager.create_stream('depth@100ms', banco.markets, stream_label='depth@100ms')
+    for moeda in banco.markets:
+        print("criando stream para", moeda)
+        binance_websocket_api_manager.create_stream('aggTrade', moeda, stream_label='aggTrade '+moeda)
 
     #binance_websocket_api_manager.start_monitoring_api(port=5013, warn_on_update=True)
 
